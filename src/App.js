99242f606213d7f439d238802d42ff0aeda16bc0
nivetha <nivetharajendran1994@gmail.com>
2023-06-01 13:16:54 +0530

    Commit message describing the changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Profile from './Profile/profile';
 // import './App.css';
-import Dashboard from './Dashboard/dashboard';
-import LoginPage from './Login/login';
+// import Dashboard from './Dashboard/dashboard';
+// import LoginPage from './Login/login';
 function App() {
   return (
     <div className="App">
