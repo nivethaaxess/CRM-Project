1a6118ca0b689e61d3793fc265cdc8436f881bf8
Kalyana-Ramanan <kalyan.durai88@gmail.com>
2023-06-21 11:50:03 +0530

    new commit changes1

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import Menu from './Dashboard/components/Menu';
 import Navbar from './Dashboard/components/Navbar';
 import Dashboard1 from './pages/Dashboard1';
 import './Dashboard/design.css'
-import User1 from './Dashboard/components/User1';
+// import User1 from './Dashboard/components/User1';
 import Content from './Dashboard/components/Content';
 import { Box } from '@mui/joy';
 import DashboardLayout from './Dashboard/DashboardLayout';
+import User1 from './pages/User1'
 import TabPanel from '../src/Profile/Tabs/tabs';
 import Profile from './Profile/profile';
 
